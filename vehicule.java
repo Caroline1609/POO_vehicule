5d318d0088feaf47b80314632a8bc018295a5f67
Caroline <sam@google.com>
2025-01-07 16:54:46 +0100

    upload

---
+++ vehicule.java
@@ -0,0 +1,49 @@
+public class vehicule {
+    
+    private String nom;
+    private String marque;
+    private String annee;
+    private int nbDeRoue;
+    private String typeCaburant;
+    private boolean moteur;
+    private boolean contact;
+    private boolean accelerateur;
+    private boolean frein;
+
+
+    public vehicule(String nom, String marque, String annee, int nbDeRoue, String typeCaburant, boolean moteur, boolean contact, boolean accelerateur, boolean frein) {
+
+        this.nom = nom;
+        this.marque = marque;
+        this.annee = annee;
+        this.nbDeRoue = nbDeRoue;
+        this.typeCaburant = typeCaburant;
+        this.moteur = moteur;
+        this.contact = contact;
+        this.accelerateur = accelerateur;
+        this.frein = frein;
+        
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} //fin de class
